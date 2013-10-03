17daa4e5cb9465839fe11fdc19ddf755773057e9
Matthew Evans <mcaevans@gmail.com>
2013-10-02 20:53:19 -0600

    Fixed merge?

--- app/js/beard.js
+++ app/js/beard.js
@@ -18,7 +18,6 @@
 			var $elm = $(elm);
 
 			// Cache other DOM elements
-<<<<<<< HEAD
 			Beard.elements             = {};
 			Beard.elements.$player     = $elm;
 			Beard.elements.$text       = $elm.find('.text');
@@ -26,13 +25,12 @@
 			Beard.elements.$progress   = $elm.find('.progress');
 			Beard.elements.$seek       = $elm.find('.seek');
 			Beard.elements.$seekCursor = $elm.find('.seek .cursor');
-=======
+
 			Beard.elements = {};
 			Beard.elements.$player = $elm;
 			Beard.elements.$text = $elm.find('.text');
 			Beard.elements.$time = $elm.find('.time');
 			Beard.elements.$play = $elm.find('.play');
->>>>>>> e17e8d3df152fa12b52afac186780f9afe3e7a0b
 
 			// Instantiate a new Audio object
 			Beard.audio = new Audio();
